537e0dad2328b5684777b3a8f017b2f71d8abcc6
unknown <binnish.rb@gmail.com>
2025-07-07 11:08:51 +0530

    This is my first version of code

--- python_app.py
+++ python_app.py
@@ -7,5 +7,9 @@ def home():
     print("Flask app is running!")
     return "Hello from Flask in VS Code!"
 
+def home():
+    print("Flask app is running!")
+    return "Hello from Flask in VS Code!"
+
 if __name__ == '__main__':
     app.run(debug=True)
